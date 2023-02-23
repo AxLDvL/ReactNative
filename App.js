dddcfbae0f00a8cc1d3473f086d06f4678ff8d9e
AxLDcode <v.michel@it-students.fr>
2023-02-23 11:43:09 +0100

    add flat list

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useState} from "react";
 
 const ListeCourse = () => {
+
   const sampleGoals = [
     "Faire les courses",
     "Aller à la salle de sport 3 fois par semaine",
@@ -15,40 +16,33 @@ const ListeCourse = () => {
     "Organiser un meetup autour de la tech",
     "Faire un triathlon",
   ];
+
+  /*
   return (
-      sampleGoals.map(item =>{
-          return (<Text> {item} </Text>)
+      sampleGoals.map((item,index )=>{
+          return (<Text key={index}> {item} </Text>)
         } )
+  )
+   */
+  return(
+  <FlatList data={sampleGoals}
+            renderItem={({item}) => <Text>{item}</Text>}
+  />
   )
   }
 
   const InputGoal = () => {
   const [Text, setText] =  useState('');
-    const sampleGoals = [
-      "Faire les courses",
-      "Aller à la salle de sport 3 fois par semaine",
-      "Monter à plus de 5000m d altitude",
-      "Acheter mon premier appartement",
-      "Perdre 5 kgs",
-      "Gagner en productivité",
-      "Apprendre un nouveau langage",
-      "Faire une mission en freelance",
-      "Organiser un meetup autour de la tech",
-      "Faire un triathlon",
-    ];
 
   return(
-      <>
+
         <TextInput
             style = {styles.input}
           placeholder="add goal to the list"
           onChangeText={newGoal => setText(newGoal)}
           defaultValue = {Text}
         />
-        <FlatList data={sampleGoals}
-                  renderItem={({item}) => <Text>{item}</Text>}
-        />
-      </>
+
   )
   }
 
@@ -56,9 +50,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <InputGoal/>
       <ListeCourse/>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -69,7 +64,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-
   },
   input:{
     paddingHorizontal: 8,
